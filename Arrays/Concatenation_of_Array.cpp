03ae377cd82ef28d27b7b6cfc7f5d25564f7d059
FINSON COUTINHO <69283994+finsoncoutinho@users.noreply.github.com>
2022-08-15 21:08:59 +0530

    Create Concatenation_of_Array.cpp

---
+++ Arrays/Concatenation_of_Array.cpp
@@ -0,0 +1,16 @@
+https://leetcode.com/problems/concatenation-of-array/
+
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int length= nums.size();
+        
+        for (int i=0; i<length;i++){
+            
+            nums.push_back(nums[i]);
+            
+        }
+        
+        return nums;
+    }
+};
